950e925bc67776a5da9057f81027939a1e2a348d
Jinjerana <125341676+Jinjerana@users.noreply.github.com>
2023-12-14 13:51:07 +0100

    corrections email

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -69,14 +69,14 @@ const signup = async (req, res) => {
 
 const verify = async (req, res) => {
 	const { verificationToken } = req.params;
-	console.log(req.params);
+
 	const user = await User.findOne({ verificationToken });
 	if (!user) {
 		throw new HttpError(404, 'User not found');
 	}
 
 	await User.findOneAndUpdate(user._id, {
-		verufy: true,
+		verify: true,
 		verificationToken: '',
 	});
 
--- helpers/sendgrid.js
+++ helpers/sendgrid.js
@@ -1,7 +1,5 @@
 import sgMail from '@sendgrid/mail';
 
-import User from '../models/users.js';
-
 import dotenv from 'dotenv';
 
 dotenv.config();
@@ -10,15 +8,15 @@ const { SENDGRID_API_TOKEN, SENDGRID_EMAIL_FROM, BASE_URL } = process.env;
 
 sgMail.setApiKey(SENDGRID_API_TOKEN);
 
-const sendgrid = (req, res) => {
-	const { verificationToken } = req.params;
-
-	const msg = {
-		to: 'jararew306@hupoi.com',
-		from: SENDGRID_EMAIL_FROM, // Use the email address or domain you verified above
-		subject: 'Test Email',
-		text: 'Nest Email with Node.js',
-		html: `<a target='_blank' href='${BASE_URL}/users/verify/${verificationToken}'>Click to verify</a>`,
+const sendgrid = () => {
+	const msg = (email, verificationToken) => {
+		return {
+			to: email,
+			from: SENDGRID_EMAIL_FROM, // Use the email address or domain you verified above
+			subject: 'Test Email',
+			text: 'Nest Email with Node.js',
+			html: `<a target='_blank' href='${BASE_URL}/users/verify/${verificationToken}'>Click to verify</a>`,
+		};
 	};
 	//ES6
 	sgMail
